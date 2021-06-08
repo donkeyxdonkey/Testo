1559c26702937559c4821dd0cefa654dcc8e56c3
Alexander Dahlkvist <alexda84@hotmail.com>
2021-06-08 21:29:03 +0200

    nya karameller

--- Testo/Testa.cs
+++ Testo/Testa.cs
@@ -24,12 +24,73 @@ namespace Testo
 
         public void testabranch(int StartIndex)
         {
-            //BYGGA EN MATRIX @KEANU
+            //BYGGA EN MATRIX @KEANU-BOI
 
             Console.WriteLine("Ange Matris storlek:>");
             var MatrixXY = Convert.ToInt32(Console.ReadLine());
 
-            var TheMatrix = new int[MatrixXY, MatrixXY];
+            var TheMatrix = new int?[MatrixXY, MatrixXY];
+            var FacingDirection = Direction.Right;
+
+            int X, Y;
+            X = Y = 0;
+
+            for (int i = 0; i < MatrixXY*MatrixXY; i++)
+            {
+                TheMatrix[X, Y] = StartIndex;
+                Console.Write(TheMatrix[X, Y]);
+                Console.ReadLine();
+
+                switch (FacingDirection)
+                {
+                    case Direction.Right:
+                        if (X == MatrixXY -1)
+                        {
+                            FacingDirection = Direction.Down;
+                            Y++;
+                        } else X++;
+
+                        break;
+                    case Direction.Down:
+                        if (Y == MatrixXY - 1)
+                        {
+                            FacingDirection = Direction.Left;
+                            X--;
+                        } else Y++;
+                        break;
+                    case Direction.Left:
+                        if (X == 0)
+                        {
+                            FacingDirection = Direction.Up;
+                            Y--;
+                        }
+                        else X--;
+                        break;
+                    case Direction.Up:
+                        if (TheMatrix[X, Y] == null)
+                        {
+                            FacingDirection = Direction.Right;
+                            X++;
+                        }
+                        else Y--;
+
+                        break;
+                }
+                
+                StartIndex++;
+
+            }
+
+            for (int i = 0; i < MatrixXY; i++)
+            {
+                for (int j = 0; j < MatrixXY; j++)
+                {
+                    Console.WriteLine(TheMatrix[j,i]);
+                }
+            }
+
+
+            Console.ReadLine();
 
         }
     }

71b0c6bf13f8bcbfbbac33347d2f6fa230ca5f22
Alexander Dahlkvist <alexda84@hotmail.com>
2021-06-08 20:16:56 +0200

    Add project files.

---
+++ Testo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var TS = new Testa();
+            TS.Run();
+        }
+    }
+}
